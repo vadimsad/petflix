4c7fcaf5953df23b98aca15b54bf2de9467c9aec
vadimsad <warff74@mail.ru>
2023-05-02 18:34:53 +0500

    fix: reduce height of quickmenu

--- src/components/Search/QuickResult/QuickResult.jsx
+++ src/components/Search/QuickResult/QuickResult.jsx
@@ -12,7 +12,7 @@ const QuickResult = React.memo(({ shown }) => {
 		<ul
 			className={`${
 				shown ? 'visible opacity-100 mt-2' : 'invisible opacity-0 mt-10'
-			} absolute z-20 right-0 md:w-[30vw] xsm:w-[300px] w-full h-[500px] py-2 rounded-xl overflow-auto border-2 border-dark dark:border-blue bg-light dark:bg-dark transition-[opacity_0.3s_ease]`}
+			} absolute z-20 right-0 md:w-[30vw] xsm:w-[300px] w-full lg:h-[500px] h-[300px] py-2 rounded-xl overflow-auto border-2 border-dark dark:border-blue bg-light dark:bg-dark transition-[opacity_0.3s_ease]`}
 		>
 			{isLoading ? (
 				[...Array(5)].map((item, index) => {
