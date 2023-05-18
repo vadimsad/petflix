0d685a7ad7cddc31ff3dbf6073c77fc071684d41
vadimsad <warff74@mail.ru>
2023-05-18 20:35:06 +0500

    feat: add award hover effect

--- src/components/SingleFilm/Awards/Awards.jsx
+++ src/components/SingleFilm/Awards/Awards.jsx
@@ -19,10 +19,10 @@ const Awards = () => {
 			<ul className='flex flex-wrap gap-4'>
 				{awards.map((award, index) => (
 					<li
-						className={`flex-[1_0_50%] lg:max-w-[150px] md:max-w-[150px] xsm:max-w-[120px] max-w-[100px] bg-darkTransparent rounded-lg xl:px-3 xl:py-4 px-1 py-2`}
+						className={`flex-[1_0_50%] lg:max-w-[150px] md:max-w-[150px] xsm:max-w-[120px] max-w-[100px] bg-darkTransparent rounded-lg xl:px-3 xl:py-4 px-1 py-2 group`}
 						key={index}
 					>
-						<div className='w-full xl:h-[100px] xsm:h-[80px] h-[60px] mb-2'>
+						<div className='w-full xl:h-[100px] xsm:h-[80px] h-[60px] mb-2 group-hover:scale-[0.95] transition-transform'>
 							<img
 								src={award.imageUrl}
 								alt={award.name}
